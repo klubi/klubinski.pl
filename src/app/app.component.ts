f7eea442abffb358575f38fc590308e774b996da
Piotr Klubinski <piotr.klubinski@jamfsoftware.com>
2017-09-23 14:13:54 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  github = 'https://github.com/klubi';
+  linkedin = 'https://www.linkedin.com/in/piotrklubinski/';
+  mail= 'pklubinski@gmail.com';
+}
